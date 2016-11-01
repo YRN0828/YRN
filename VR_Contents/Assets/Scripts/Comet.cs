b2e48fa72e45272991ec753bd668468f3867b172
YRN0828 <skrbkmt@yahoo.co.jp>
2016-11-01 15:54:25 +0900

    1st

---
+++ VR_Contents/Assets/Scripts/Comet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Comet : MonoBehaviour {
+
+    public GameObject playerObject;
+    public float destroyTime;
+
+	// Use this for initialization
+	void Start () {
+        //playerObject = GameObject.Find("Player");
+        //Destroy(gameObject, destroyTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    /*if(Vector3.Distance(playerObject.transform.position,this.transform.position) >= 50.0f)
+        {
+            Destroy(this.gameObject);
+        }*/
+	}
+}
