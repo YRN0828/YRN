5a18d21e056895b69c72851200ed81b3345caee4
YRN0828 <skrbkmt@yahoo.co.jp>
2016-11-05 08:22:45 +0900

    2nd
    微調整、修正、一部テスト

--- VR_Contents/Assets/Scripts/CreateComet.cs
+++ VR_Contents/Assets/Scripts/CreateComet.cs
@@ -5,6 +5,10 @@ public class CreateComet : MonoBehaviour {
 
     public GameObject playerObject;
     public GameObject cometPrefab;
+
+    private GameObject comet;
+    public GameObject comet_;
+
     private GameObject[] mkComet;
     public int cometCount = 0;
     public int spawnCount;
@@ -14,43 +18,33 @@ public class CreateComet : MonoBehaviour {
     public float rangeY = 5.0f;
     public float rangeZ = 1.0f;
 
+    private CombineMesh combine;
+
     public bool startTF = false;
 
     public float playerZ;
 
 	// Use this for initialization
 	void Start () {
-	    
-	}
+        comet_ = GameObject.Find("Comet_");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        //cometCount++;
-        if(/*cometCount >= spawnCount*/ cometCount < mkInt)
+        if(cometCount < mkInt)
         {
-            playerZ = playerObject.transform.position.z; //1個目生成
-            transform.position = new Vector3(Random.Range(-spawnPoint, spawnPoint), Random.Range(-spawnPoint, spawnPoint), Random.Range(-spawnPoint, spawnPoint));
-            Debug.Log(transform.position);
-            Instantiate(cometPrefab, transform.position, transform.rotation);
-
-            playerZ = playerObject.transform.position.z; //2個目生成
-            transform.position = new Vector3(Random.Range(-spawnPoint, spawnPoint), Random.Range(-spawnPoint, spawnPoint), Random.Range(-spawnPoint, spawnPoint));
-            Debug.Log(transform.position);
-            Instantiate(cometPrefab, transform.position, transform.rotation);
-
-            playerZ = playerObject.transform.position.z; //3個目生成
-            transform.position = new Vector3(Random.Range(-spawnPoint, spawnPoint), Random.Range(-spawnPoint, spawnPoint), Random.Range(-spawnPoint, spawnPoint));
-            Debug.Log(transform.position);
-            Instantiate(cometPrefab, transform.position, transform.rotation);
-
-            playerZ = playerObject.transform.position.z; //4個目生成
-            transform.position = new Vector3(Random.Range(-spawnPoint, spawnPoint), Random.Range(-spawnPoint, spawnPoint), Random.Range(-spawnPoint, spawnPoint));
-            Debug.Log(transform.position);
-            Instantiate(cometPrefab, transform.position, transform.rotation);
-
+            for (int i = 0; i < 4; i++)
+            {
+                playerZ = playerObject.transform.position.z; //4個生成
+                transform.position = new Vector3(Random.Range(-spawnPoint, spawnPoint), Random.Range(-spawnPoint, spawnPoint), Random.Range(-spawnPoint, spawnPoint));
+                Debug.Log(transform.position);
+                comet = (GameObject)Instantiate(cometPrefab, transform.position, transform.rotation);
+                comet.transform.parent = comet_.transform;
+            }
 
             //cometCount = 0;
             cometCount +=4;
+            
         }
         else
         {
